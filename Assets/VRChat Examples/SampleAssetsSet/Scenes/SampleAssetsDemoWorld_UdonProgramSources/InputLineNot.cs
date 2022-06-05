6ee6ff4446e259e2a6ec948976ec2436cb1d6f8c
Robert Messer <69157624+rkylem@users.noreply.github.com>
2022-06-05 10:58:17 -0400

    Initial Commit, started late. Switch and Not gate created. Recently broke the line renders which is why I created the repo

---
+++ Assets/VRChat Examples/SampleAssetsSet/Scenes/SampleAssetsDemoWorld_UdonProgramSources/InputLineNot.cs
@@ -0,0 +1,65 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+public class InputLineNot : UdonSharpBehaviour
+{
+    public NotGate notGate;
+    float countDownTimer;
+    public float timeDelayToUpdate = 0.1f;
+    bool startedTimer = false;
+    bool inputSignal = false;
+    bool inUse = false;
+
+    void Start()
+    {
+        countDownTimer = timeDelayToUpdate;
+    }
+    private void Update()
+    {
+        // update delay
+        if (startedTimer)
+        {
+            countDownTimer -= Time.deltaTime;
+            if (countDownTimer <= 0)
+            {
+                countDownTimer = timeDelayToUpdate;
+                startedTimer = false;
+                SendUpdate();
+            }
+        }
+    }
+    // start timer to send the update
+    public void UpdateGate()
+    {
+        startedTimer = true;
+    }
+    void SendUpdate()
+    {
+        if (inputSignal)
+        {
+            notGate.OnTrue();
+        }
+        else
+        {
+            notGate.OnFalse();
+        }
+        notGate.OnPickupUseDown();
+    }
+
+    public bool GetInputSignal()
+    {
+        return inputSignal;
+    }
+    public void SetInputSignal(bool inSignal)
+    {
+        inputSignal = inSignal;
+    }
+    public bool GetInUse()
+    {
+        return inUse;
+    }
+    public void SetInUse(bool _inUse)
+    {
+        inUse = _inUse;
+    }
+}
---
+++ Assets/VRChat Examples/SampleAssetsSet/Scenes/SampleAssetsDemoWorld_UdonProgramSources/NotGate.cs
@@ -0,0 +1,72 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class NotGate : UdonSharpBehaviour
+{
+    public GameObject on;
+    public GameObject off;
+    public LineRenderer powerLine;
+    public Material green;
+    public Material red;
+
+    private void Start()
+    {
+        powerLine.material = red;
+    }
+    // If player moves the Gate, disconnect
+    public override void OnPickup()
+    {
+        InputLineNot input = GetComponentInChildren<InputLineNot>();
+        if (input)
+        {
+            input.SetInUse(false);
+            input.SetInputSignal(false);
+        }
+    }
+    public override void OnPickupUseDown()
+    {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Inverter");
+    }
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        if (Networking.IsMaster)
+        {
+            if (on.activeSelf)
+            {
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "OnTrue");
+            }
+            else
+            {
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "OnFalse");
+            }
+        }
+    }
+    public void Inverter()
+    {
+        on.SetActive(!on.activeSelf);
+        off.SetActive(!off.activeSelf);
+        if (on.activeSelf)
+        {
+            powerLine.material = green;
+        }
+        else
+        {
+            powerLine.material = red;
+        }
+    }
+
+    public void OnTrue()
+    {
+        on.SetActive(true);
+        off.SetActive(false);
+        powerLine.material = green;
+    }
+    public void OnFalse()
+    {
+        on.SetActive(false);
+        off.SetActive(true);
+        powerLine.material = red;
+    }
+}
