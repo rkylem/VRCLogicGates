1ad7ead9723142d63d08755c83b706b08994a89e
Robert Messer <69157624+rkylem@users.noreply.github.com>
2022-06-09 23:59:44 -0400

    Working not gates and quest compatible. Also I messed with a lot of the materials and delete some. I didn't see any visible changes, but who knows.

---
+++ Assets/VRChat Examples/SampleAssetsSet/Scenes/SampleAssetsDemoWorld_UdonProgramSources/Logic Scripts/NotGate.cs
@@ -0,0 +1,105 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class NotGate : UdonSharpBehaviour
+{
+    public Material green;
+    public Material red;
+    public GameObject on;
+    public GameObject off;
+    public LineRenderer powerLine;
+    public PowerLineMover powerLineScript;
+    public InputLineNot input;
+
+    // If player moves the Gate, disconnect
+    public override void OnPickup()
+    {
+        // the on powerLineScript Onpickup function disconnects the line from the gate,
+        // we can use that here
+        //InputLineNot input = GetComponentInChildren<InputLineNot>();
+        if (input)
+        {
+            input.SetInUse(false);
+            input.SetInputSignal(false);
+            input.ForceUpdateGate();            
+        }
+    }
+    //public void TempPickUp()
+    //{
+    //    InputLineNot input = GetComponentInChildren<InputLineNot>();
+    //    if (input)
+    //    {
+    //        input.SetInUse(false);
+    //        input.SetInputSignal(false);
+    //        input.UpdateGate();
+    //    }
+    //}
+    public override void OnPickupUseDown()
+    {// seems like these netowrk even can only call public functions
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "Invert");
+    }
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        if (Networking.IsMaster)
+        {
+            if (on.activeSelf)
+            { // might be better to update just the joined player somehow
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "OnTrue");
+            }
+            else
+            {
+                SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "OnFalse");
+            }
+        }
+    }
+    public void Invert()
+    {
+        on.SetActive(!on.activeSelf);
+        off.SetActive(!off.activeSelf);
+        if (on.activeSelf)
+        {
+            powerLine.material = green;
+        }
+        else
+        {
+            powerLine.material = red;
+        }
+        // Update gate if pickedup
+        //InputLineNot input = GetComponentInChildren<InputLineNot>();
+        if (input && !input.GetInUse())
+        {
+            input.SetInputSignal(on.activeSelf);
+            input.UpdateGate();
+        }
+        //send singal over the powerline
+        powerLineScript.SendSignalUpdate();
+    }
+
+    // might need to do the Networking.Ismaster for these
+    public void NetworkedOnTrue()
+    {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "OnTrue");
+    }
+    public void NetworkedOnFalse()
+    {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "OnFalse");
+    }
+    public void OnTrue()
+    {
+        on.SetActive(true);
+        off.SetActive(false);
+        powerLine.material = green;
+
+        powerLineScript.SendSignalUpdate();
+    }
+    public void OnFalse()
+    {
+        on.SetActive(false);
+        off.SetActive(true);
+        powerLine.material = red;
+
+        powerLineScript.SendSignalUpdate();
+    }
+}
---
+++ Assets/VRChat Examples/SampleAssetsSet/Scenes/SampleAssetsDemoWorld_UdonProgramSources/Logic Scripts/PowerLineMover.cs
@@ -0,0 +1,134 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+public class PowerLineMover : UdonSharpBehaviour
+{
+    public LineRenderer powerLine;
+    public GameObject notGateObjects;
+    public GameObject on;
+    public float timeDelayToActivate = 0.3f;
+    InputLineNot connectedInput;
+    float countDownTimer;
+    bool startedTimer = false;
+
+    public GameObject[] inputs;
+    //public GameObject[] inputs = new GameObject[1];
+    private void Start()
+    {
+        // TODO find all the inputs
+        // manually adding them
+        // will likely need to update the list if I add more in the world dynamically.
+        // if I can somehow use the start() for the InputLines to add them, that should work better.
+        // I think if I had an array for all the powerlines and all the inputs I should be able
+        // to make them update dynamically.
+        //inputs[0] = GameObject.Find("Input Line");
+        countDownTimer = timeDelayToActivate;
+    }
+
+    private void Update()
+    {
+        // set powerline positions according to the parent and mover(self) positions
+        powerLine.SetPosition(0, notGateObjects.transform.position - transform.parent.GetComponent<Transform>().position);
+        powerLine.SetPosition(1, transform.position - transform.parent.GetComponent<Transform>().position);
+        // input delay
+        if (startedTimer)
+        {
+            countDownTimer -= Time.deltaTime;
+            if (countDownTimer <= 0)
+            {
+                countDownTimer = timeDelayToActivate;
+                startedTimer = false;
+                SendSignalUpdate();
+            }
+        }
+    }
+
+    // If player moves the powerline, disconnect
+    public override void OnPickup()
+    {
+        startedTimer = false;
+        countDownTimer = timeDelayToActivate;
+        if (connectedInput)
+        {
+            connectedInput.SetInputSignal(false);
+            connectedInput.SetInUse(false);
+            connectedInput.ForceUpdateGate();
+            connectedInput = null;
+        }
+    }
+    //public void TempPickUp()
+    //{
+    //    countDownTimer = timeDelayToActivate;
+    //    startedTimer = false;
+    //    if (connectedInput)
+    //    {
+    //        connectedInput.SetInUse(false);
+    //        connectedInput.SetInputSignal(false);
+    //        SendSignalUpdate();
+    //        connectedInput = null;
+    //    }
+    //}
+    public override void OnDrop()
+    {
+        // try to connect to an imput.
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            // if input is not in use and less than 0.2 units away connect
+            if (!inputs[i].GetComponent<InputLineNot>().GetInUse() &&
+                Vector3.Distance(transform.position, inputs[i].transform.position) < 0.2f)
+            {
+                startedTimer = true;
+                connectedInput = inputs[i].GetComponent<InputLineNot>();
+                connectedInput.SetInUse(true);
+                transform.position = inputs[i].transform.position;
+                transform.rotation = inputs[i].transform.rotation;
+                break;
+            }
+        }
+    }
+
+    //public void ConnectToInput()
+    //{
+    //    for (int i = 0; i < inputs.Length; i++)
+    //    {
+    //        // if input is not in use and less than 0.2 units away connect
+    //        if (!inputs[i].GetComponent<InputLineNot>().GetInUse() &&
+    //            Vector3.Distance(transform.position, inputs[i].transform.position) < 0.2f)
+    //        {
+    //            startedTimer = true;
+    //            connectedInput = inputs[i].GetComponent<InputLineNot>();
+    //            connectedInput.SetInUse(true);
+    //            transform.position = inputs[i].transform.position;
+    //            transform.rotation = inputs[i].transform.rotation;
+    //            break;
+    //        }
+    //    }
+    //}
+
+    public void SendSignalUpdate()
+    {
+        // null check should be needed if input line was removed(pickedUp) before timer ended. and maybe for late jioners
+        if (connectedInput)
+        {
+            connectedInput.SetInputSignal(on.activeSelf);
+            connectedInput.UpdateGate();
+        }
+    }
+
+    //GameObject[] FindGameObjectsWithName(string name)
+    //{
+    //    int a = GameObject.FindObjectsOfType<GameObject>().Length;
+    //    GameObject[] arr = new GameObject[a];
+    //    int FluentNumber = 0;
+    //    for (int i = 0; i < a; i++)
+    //    {
+    //        if (GameObject.FindObjectsOfType<GameObject>()[i].name == name)
+    //        {
+    //            arr[FluentNumber] = GameObject.FindObjectsOfType<GameObject>()[i];
+    //            FluentNumber++;
+    //        }
+    //    }
+    //    System.Array.Resize(ref arr, FluentNumber);
+    //    return arr;
+    //}
+}    
